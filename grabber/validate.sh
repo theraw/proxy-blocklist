51521838adb8dd9e8dc5d6934b17501224a5a05f
𝓙𝓾𝓵𝓲𝓸 <daraw665@gmail.com>
2024-07-05 21:58:22 +0200

    Update validate.sh

--- grabber/validate.sh
+++ grabber/validate.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-rm -rf valid.txt
-# Define a pattern for a valid IP address
-pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
-
-# Read the list of IP addresses from a file
-ips=($(cat unique_list.txt))
-
-# Loop through each IP address in the list
-for ip in "${ips[@]}"; do
-  # Check if the IP address matches the pattern
-  if [[ $ip =~ $pattern ]]; then
-    # Check if the IP address falls within a valid range
-    IFS=. read -a octets <<< "$ip"
-    if [[ ${octets[0]} -le 255 && ${octets[1]} -le 255 && ${octets[2]} -le 255 && ${octets[3]} -le 255 &&
-      ${octets[0]} -ge 0 && ${octets[1]} -ge 0 && ${octets[2]} -ge 0 && ${octets[3]} -ge 0 &&
-      ${octets[0]} != 127 && ${octets[0]} != 10 && 
-      (${octets[0]} != 172 || (${octets[1]} -ge 16 && ${octets[1]} -le 31)) &&
-      (${octets[0]} != 192 || ${octets[1]} != 168) &&
-      (${octets[0]} -lt 224) ]]; then
-      # If it is a valid IP, keep it in the list
-      valid_ips+=($ip)
-    fi
-  fi
-done
-
-# Write the valid IP addresses to a file
-for ip in "${valid_ips[@]}"; do
-  echo $ip >> valid.txt
-done
-rm -Rf unique_list.txt
+input="unique_list.txt"
+output="valid.txt"
+rm -rf "$output"
+awk '
+{
+  split($0, octets, ".")
+  if (length(octets) == 4 && octets[1] ~ /^[1-9][0-9]*$/ && octets[2] ~ /^[0-9]+$/ && octets[3] ~ /^[0-9]+$/ && octets[4] ~ /^[0-9]+$/ &&
+      octets[1] <= 223 && octets[2] <= 255 && octets[3] <= 255 && octets[4] <= 255 &&
+      !(octets[1] == 127) && !(octets[1] == 10) &&
+      !(octets[1] == 192 && octets[2] == 168) &&
+      !(octets[1] == 172 && octets[2] >= 16 && octets[2] <= 31)) {
+    print $0
+  } else {
+    # debug purposes in cases weird ips are going to valid list
+    # print "Invalid format or range: " $0 > "/dev/stderr"
+  }
+}
+' "$input" > "$output"
+rm -Rf "$input"
